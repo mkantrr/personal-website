ec800bdb345e12b126953db383f589c7627e6a92
Matthew Kanter <mattkanter1@outlook.com>
2024-02-10 13:24:49 -0500

    quick fix for format, content placeholder for portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,32 +2,19 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Hidden } from '@material-ui/core';
 import DisplacementSphere from '../components/background/DisplacementSphere';
+import { Content } from '../components/content/Content';
 import { LogoLink } from '../components/logo/LogoLink';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
 import { FooterText } from '../components/footer/FooterText';
 import { SpeedDials } from '../components/speedDial/SpeedDial';
 import { PortfolioIcons } from '../components/content/PortfolioIcons';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
     minHeight: '100vh',
-  },
-  socialIcons: {
-    position: 'absolute',
-    top: theme.spacing(6),
-    right: theme.spacing(6),
-  },
-  iconButton: {
-    height: '2.5rem',
-    width: '2.5rem',
-    display: 'block',
-    marginBottom: theme.spacing(2),
-  },
-  icon: {
-    fontSize: '1.25rem',
-  },
+  }
 }));
 
 export const Portfolio = () => {
@@ -38,6 +25,7 @@ export const Portfolio = () => {
       <div className={classes.root}>
         <DisplacementSphere />
         <LogoLink />
+        <Content />
         <ThemeToggle />
         <Hidden smDown>
          <PortfolioIcons /> 
